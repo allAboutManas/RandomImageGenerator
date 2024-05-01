5e7d8d2a25aaec47793ea3731d302b866bc4f835
allAboutManas <rm.manasraj@gmail.com>
2024-05-01 10:00:29 +0530

    firstcommit

---
+++ src/component/Imgcard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./imgcard.module.css";
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
+export default function Imgcard() {
+    const [data, setData] = useState();
+  const[value,setValue]=useState(0)
+
+    function handleClick(){
+        setValue(value+1)
+        
+    }
+    useEffect(()=>{
+        async function fetchData(){
+          let response = await axios.get('https://dog.ceo/api/breeds/image/random')
+          setData(response.data.message);
+          
+        }
+        fetchData();
+      },[value])
+   
+
+  return (
+    <div>
+      <div className={styles.crd}>
+        <img src={data} width="100%" alt="" />
+        <p className={styles.para}>Bhudeo</p>
+        <button onClick={handleClick}>Change Bholu
+      </button>
+      </div>
+      
+    </div>
+  );
+}
